c82090a5aa7af95bde82ff34af0973600de7a76d
AnetM <mudruk.anya@icloud.com>
2022-09-14 11:52:11 +0300

    updated

---
+++ js/task-3.js
@@ -0,0 +1,21 @@
+const findLongestWord = function(string) {
+   
+    const arr = string.split(" ");
+    let longestWord = arr[0];
+    
+    for (let i = 0; i <= arr.length - 1; i += 1) {
+        if (longestWord.length < arr[i].length) {
+            longestWord = arr[i];
+        }
+    }
+    return longestWord;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+console.log(findLongestWord('Google do a roll')); // 'Google'
+
+console.log(findLongestWord('May the force be with you')); // 'force'
---
+++ js/task-6.js
@@ -0,0 +1,34 @@
+let input;
+const numbers = [];
+let total = 0;
+
+input = prompt("Введіть число");
+
+
+
+console.log(input)
+if (input === null) {
+    total += input;
+} else {
+numbers.push(input);
+    console.log(numbers)
+}
+
+if (input !== null) {
+    input = prompt("Введіть число");
+}
+
+// for (let input = ! null) {
+//     numbers.push(input);
+//     console.log(numbers)
+// }
+
+
+
+
+
+
+
+
+
+
